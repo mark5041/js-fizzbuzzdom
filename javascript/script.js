0a4474b248dfed47fd8e9145dcb444e546a63860
mark5041 <mark.beloso5041@gmail.com>
2021-11-23 15:45:39 +0100

    square with content inside

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,37 @@
+let container = document.querySelector(".container");
+let row = document.createElement("div");
+row.classList.add("row");
+let element;
+let col;
+let insidetext
+
 for(let i = 1; i <= 100; i++)
 {
-    
+    col = document.createElement("div");
+    col.classList.add("box","mx-1","my-1");
+
+    switch(true)
+    {
+        case i % 3 == 0 && i % 5 == 0:
+            element = "FizzBuzz";
+            col.classList.add("my-bg-red");
+        break;
+        case i % 3 == 0:
+            element = "Fizz";
+            col.classList.add("my-bg-green");
+        break;
+        case i % 5 == 0:
+            element = "Buzz";
+            col.classList.add("my-bg-yellow");
+        break;
+        default: 
+            element = i;
+            col.classList.add("my-bg-blue");
+    }
+
+    insidetext = `<span> ${element} </span>`;
+    col.innerHTML = insidetext;
+    row.append(col);
 }
+
+container.append(row);
